9658330d138489cdfcf0da9179e73d9cbaf23bdb
achmadqizwini <achmadqizwini4@gmail.com>
2025-01-19 23:25:05 +0700

    Add: mini calendar

--- mini_calendar/index.js
+++ mini_calendar/index.js
@@ -1,18 +1,16 @@
-const monthNameEl = document.getElementById("month-name");
-const dayNameEl = document.getElementById("day-name");
-const dayNumEl = document.getElementById("day-number");
+const monthNameEl = document.getElementById("month");
+const dayNameEl = document.getElementById("day");
+const dayNumEl = document.getElementById("date");
 const yearEl = document.getElementById("year");
 
 const date = new Date();
-const month = date.getMonth();
 monthNameEl.innerText = date.toLocaleString("en", {
   month: "long",
 });
 
 dayNameEl.innerText = date.toLocaleString("en", {
-  weekday: "long",
+    weekday: "long"
 });
-
 dayNumEl.innerText = date.getDate();
 
 yearEl.innerText = date.getFullYear();
