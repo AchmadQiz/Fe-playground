4f1a222ef6595582cf990ae7a7dad4080764c682
achmadqizwini <achmadqizwini4@gmail.com>
2025-02-01 17:00:53 +0700

    Add: random photos

--- random_photos/index.js
+++ random_photos/index.js
@@ -1,18 +1,16 @@
-const imageContainerEl = document.querySelector(".image-container");
+const imageContainerEl = document.querySelector(".img-container")
+const buttonEl = document.querySelector(".btn")
 
-const btnEl = document.querySelector(".btn");
+buttonEl.addEventListener("click", () => {
+    let num = 10;
+    addNewImages(num)
+})
 
-btnEl.addEventListener("click", () => {
-  imageNum = 10;
-  addNewImages();
-});
-
-function addNewImages() {
-  for (let index = 0; index < imageNum; index++) {
-    const newImgEl = document.createElement("img");
-    newImgEl.src = `https://picsum.photos/300?random=${Math.floor(
-      Math.random() * 2000
-    )}`;
-    imageContainerEl.appendChild(newImgEl);
-  }
+function addNewImages(num) {
+    for (i = 0; i <= num; i++) {
+       let img = document.createElement("img")
+       img.src = `https://picsum.photos/300?random=${Math.random() * 2000}`
+       console.log(img)
+       imageContainerEl.appendChild(img)
+    }
 }
